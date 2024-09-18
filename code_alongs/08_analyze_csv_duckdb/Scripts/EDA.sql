a6586d2a53731d7fff9cdd3911a50c845bec1bb4
Erik Johansson <erik.elias.johansson@gmail.com>
2024-09-18 12:04:37 +0200

    social media database init

---
+++ code_alongs/08_analyze_csv_duckdb/Scripts/EDA.sql
@@ -0,0 +1,19 @@
+SELECT * FROM youtube;
+
+SELECT DISTINCT(category) from youtube;
+
+SELECT COUNT(*) FROM youtube;
+
+-- how many in each category top 10?
+
+SELECT
+	category,
+	COUNT(*) AS Number
+FROM
+	youtube
+GROUP BY
+	category
+ORDER BY
+	number DESC
+LIMIT 10;
+	
